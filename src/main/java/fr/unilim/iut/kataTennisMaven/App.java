1716f9775ea3b9f08a0826b1c635c3de1fdb1217
clement <clement@164.81.236.60>
2017-11-06 11:07:09 +0100

    premier commit : code à réfactorer.

---
+++ src/main/java/fr/unilim/iut/kataTennisMaven/App.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.kataTennisMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/fr/unilim/iut/kataTennisMaven/TennisGame.java
@@ -0,0 +1,6 @@
+package fr.unilim.iut.kataTennisMaven;
+
+public interface TennisGame {
+    void wonPoint(String playerName);
+    String getScore();
+}
